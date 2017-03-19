91824c7edb7a258a5e9a2c44bb8736a34af502a4
uecm <uecmegor@gmail.com>
2017-03-19 15:28:21 +0200

    Added language choosing feature.

--- RandomShake/ViewController.m
+++ RandomShake/ViewController.m
@@ -10,12 +10,7 @@
 #import "WebPage.h"
 
 
-// If web page is loaded not because of shake, which means it was already loaded
-// and saved to the array, it should not trigger webViewDidFinishLoad: method
-// to save this page once more.
-
-
-@interface ViewController () <UIWebViewDelegate, UIGestureRecognizerDelegate> {
+@interface ViewController () <UIWebViewDelegate, UIGestureRecognizerDelegate, UIPickerViewDelegate, UIPickerViewDataSource> {
 
     BOOL loaded;
     BOOL didEdgePan;
@@ -24,11 +19,18 @@
     
     IBOutlet UIProgressView* progressView;
     NSTimer *progressTimer;
-    __weak IBOutlet UILabel *shakeLabel;
     
+    __weak IBOutlet UILabel *shakeLabel;
+    __weak IBOutlet UIButton *languageButton;
+    __weak IBOutlet UIPickerView *languagePicker;
+    __weak IBOutlet UIButton *doneButton;
+
     UIView *maskView;
     NSTimer *maskTimer;
     
+    NSArray *languages;
+    NSArray *langPrefixes;
+    NSString *prefix;
 }
 
 @end
@@ -43,9 +45,14 @@
     [progressView setProgress:0.0f];
     [_webView setDelegate:self];
     
+    // Color of wikipedia header, use as background for views
+    UIColor *backgroundColor = [UIColor colorWithRed:235.0f/255.0f green:236.0f/255.0f blue:240.0f/255.0f alpha:1.0f];
+    [self.view setBackgroundColor:backgroundColor];
+    [_webView setBackgroundColor:backgroundColor];
+    [_webView.scrollView setBackgroundColor:backgroundColor];
+    
     // Index of current page in Web Pages array
     index = -1;
-    
     webPages = [[NSMutableArray alloc] init];
     
     // Setup left gesture recognizer
@@ -57,6 +64,26 @@
     UIScreenEdgePanGestureRecognizer *rightPanGestureRecognizer = [[UIScreenEdgePanGestureRecognizer alloc] initWithTarget:self action:@selector(rightEdgePanGestureRecognizer:)];
     [rightPanGestureRecognizer setEdges:UIRectEdgeRight];
     [self.view addGestureRecognizer:rightPanGestureRecognizer];
+    
+    // Add refresh subview
+    UIRefreshControl *refreshControl = [[UIRefreshControl alloc] init];
+    [refreshControl addTarget:self action:@selector(refreshPage:) forControlEvents:UIControlEventValueChanged];
+    [refreshControl setBackgroundColor:backgroundColor];
+    NSMutableAttributedString *refreshTitle = [[NSMutableAttributedString alloc] initWithString:@"Pull to Refresh"];
+    [refreshTitle addAttribute:NSFontAttributeName value:[UIFont fontWithName:@"HoeflerText-Regular" size:17.0f ] range:NSMakeRange(0, [refreshTitle.string length])];
+    [refreshControl setAttributedTitle:refreshTitle];
+    
+    [self.webView.scrollView addSubview:refreshControl];
+    
+    // Change status bar color
+    UIView *statusBar = [[[UIApplication sharedApplication] valueForKey:@"statusBarWindow"] valueForKey:@"statusBar"];
+    if ([statusBar respondsToSelector:@selector(setBackgroundColor:)]) {
+        statusBar.backgroundColor = backgroundColor;
+    }
+    
+    // Language picker setup
+    [languagePicker setDataSource:self];
+    [languagePicker setDelegate:self];
     
 }
 
@@ -79,6 +106,23 @@
     [super viewWillDisappear:animated];
 }
 
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
+    return 1;
+}
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
+    if (languages == nil) {
+        return 0;
+    }
+    return languages.count;
+}
+
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
+    if (languages == nil) {
+        return @"";
+    }
+    return languages[row];
+}
+
 
 #pragma mark - Gestures and motion handle
 
@@ -86,19 +130,17 @@
     if ([_webView isLoading]) {
         return;
     }
-    if (motion == UIEventSubtypeMotionShake) {
+    if (motion == UIEventSubtypeMotionShake && languagePicker.isHidden) {
+        
+        [languageButton setHidden:true];
         [_webView setHidden:false];
         [_webView loadRequest:[self randomWikipediaPageRequest]];
     }
 }
-
 -(void)leftEdgePanGestureRecognizer:(UIScreenEdgePanGestureRecognizer*)gesture{
-   NSLog(@"works from left side");
-    
     if (index == -1) {
         return;
     }
-
     if (gesture.state == UIGestureRecognizerStateBegan) {
         
         if (maskView) {
@@ -130,7 +172,6 @@
         if (index <= 0 || maskView.frame.size.width < 50) {
             return;
         }
-        
         index--;
 
         WebPage *prevPage = [webPages objectAtIndex:index];
@@ -138,18 +179,12 @@
         
         didEdgePan = true;
         [_webView loadRequest:request];
-
     }
 }
-
-
 -(void)rightEdgePanGestureRecognizer:(UIScreenEdgePanGestureRecognizer*)gesture{
-    NSLog(@"works from right side");
-   
     if (index == -1) {
         return;
     }
-    
     if (gesture.state == UIGestureRecognizerStateBegan) {
         if (maskView) {
             [maskView removeFromSuperview];
@@ -181,7 +216,6 @@
         if (maskView.frame.size.width < 50 || index >= webPages.count - 1) {
             return;
         }
-        
         index++;
         WebPage *nextPage = [webPages objectAtIndex:index];
         NSURLRequest *request = [NSURLRequest requestWithURL:nextPage.url];
@@ -191,14 +225,41 @@
     }
 }
 
--(void)handleScreenEdgePanGesture{
+- (IBAction)languageButtonPressed:(id)sender {
     
+    [self parseLanguagesFromFile];
+    
+    [languagePicker reloadAllComponents];
+    [languagePicker setHidden:false];
+    [doneButton setHidden:false];
+    [shakeLabel setHidden:true];
+    
+    [languagePicker selectRow:6 inComponent:0 animated:0];
+    [languagePicker reloadInputViews];
+
 }
+
+- (IBAction)doneButtonPressed:(id)sender {
+    [doneButton setHidden:true];
+    [languagePicker setHidden:true];
+    [shakeLabel setHidden:false];
+    
+    NSString *selectedLanguage = [languages objectAtIndex:[languagePicker selectedRowInComponent:0]];
+    prefix = [langPrefixes objectAtIndex:[languagePicker selectedRowInComponent:0]];
+    [languageButton setTitle:selectedLanguage forState:UIControlStateNormal];
+}
+
 
 #pragma mark - Misc
 
 -(NSURLRequest *)randomWikipediaPageRequest{
-    NSURL *url = [NSURL URLWithString:@"https://en.m.wikipedia.org/wiki/Special:Random"];
+    
+    NSString *URLString = @"https://en.m.wikipedia.org/wiki/Special:Random";
+    
+    if (prefix) {
+        URLString = [NSString stringWithFormat:@"https://%@.m.wikipedia.org/wiki/Special:Random", prefix];
+    }
+    NSURL *url = [NSURL URLWithString:URLString];
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
     return request;
 }
@@ -212,8 +273,6 @@
     loaded = false;
     //0.01667 is roughly 1/60, so it will update at 60 FPS
     progressTimer = [NSTimer scheduledTimerWithTimeInterval:0.01667 target:self selector:@selector(timerCallback) userInfo:nil repeats:YES];
-    
-    
 }
 
 - (void)webViewDidFinishLoad:(UIWebView *)webView{
@@ -226,15 +285,12 @@
     if (!didEdgePan){
         index++;
         if ([webPages lastObject]) {
-            int length = webPages.count;
+            int length = (int)webPages.count;
             for (int i = index; i < length; i++) {
                 [webPages removeObjectAtIndex:index];
             }
         }
-        
         WebPage *webPage = [[WebPage alloc] init];
-        
-        // An issue may be there
         [webPage setUrl:_webView.request.URL];
         
         if ([webPages lastObject]) {
@@ -242,10 +298,8 @@
             [[webPages objectAtIndex:index-1] setNext:webPage];
         }
         [webPages addObject:webPage];
-    
     }
     didEdgePan = false;
-    
 }
 
 -(void)timerCallback {
@@ -275,6 +329,51 @@
         [maskView removeFromSuperview];
         [maskTimer invalidate];
     }
+}
+
+-(void) refreshPage:(UIRefreshControl*) refresh{
+    [self.webView reload];
+    // Actually no edge pan gestrue was made when this code is called,
+    // bringing it to true will cause a page not to save once again to the queue.
+    didEdgePan = true;
+    [refresh endRefreshing];
+}
+
+-(void) parseLanguagesFromFile{
+    
+    //Parsing languages, that wikipedia supports from file.
+    //Languages list is stored in 'languages' array,
+    //Website prefixes list is stored in 'langPrefixes' array.
+    
+    NSError *error;
+    
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"Languages" ofType:nil];
+    NSString *fileContents = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:&error];
+    
+    NSArray *allStrings = [fileContents componentsSeparatedByCharactersInSet:[NSCharacterSet newlineCharacterSet]];
+    
+    NSMutableArray *plainLangs = [[NSMutableArray alloc] init];
+    NSMutableArray *prefixes = [[NSMutableArray alloc] init];
+    
+    for (NSString *lang in allStrings) {
+        
+        // Parse plain language from the line
+        NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"\\|.[^\\|]*\\}\\}" options:0 error:&error];
+        NSRange range = [regex rangeOfFirstMatchInString:lang options:0 range:NSMakeRange(0, [lang length])];
+        
+        NSString *plainLanguage = [lang substringWithRange:NSMakeRange(range.location+1, range.length-3)];
+        [plainLangs addObject:plainLanguage];
+        
+        // Parse its prefix
+        regex = [NSRegularExpression regularExpressionWithPattern:@"\\{\\{.[^\\|]*\\|" options:0 error:&error];
+        range = [regex rangeOfFirstMatchInString:lang options:0 range:NSMakeRange(0, [lang length])];
+        
+        NSString *prefixString = [lang substringWithRange:NSMakeRange(range.location + 2, range.length - 3)];
+        [prefixes addObject:prefixString];
+    }
+    
+    languages = plainLangs;
+    langPrefixes = prefixes;
 }
 
 @end
